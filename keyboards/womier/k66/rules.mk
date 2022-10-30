be56d8c995eda4d4a1e9bbb5ca364b45f54a212e
Lolepopie <8401103+lolepop@users.noreply.github.com>
2022-10-30 16:37:18 +0800

    .

--- keyboards/womier/k66/rules.mk
+++ keyboards/womier/k66/rules.mk
@@ -1,9 +1,9 @@
 # project specific files
-# SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
-# SRC += rgb_matrix.c #RGB Matrix driver
-# SRC += config_led.c #LED Matrix config
-# SRC += underglow.c  #to control underglow LEDs via I2C
-# SRC += rand.c		#replace rand() with something smaller
+SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
+SRC += rgb_matrix.c #RGB Matrix driver
+SRC += config_led.c #LED Matrix config
+SRC += underglow.c  #to control underglow LEDs via I2C
+SRC += rand.c		#replace rand() with something smaller
 
 # MCU name
 MCU = SN32F268F
@@ -24,7 +24,7 @@ NKRO_ENABLE = yes        	# USB Nkey Rollover
 KEYBOARD_SHARED_EP = no		# not needed, no = better BIOS support
 AVR_USE_MINIMAL_PRINTF = yes
 CUSTOM_MATRIX = yes			# custom key matrix driver, since COLs are shared with LED ON/OFF
-RGB_MATRIX_ENABLE = no		# use RGB Matrix
+RGB_MATRIX_ENABLE = yes		# use RGB Matrix
 RGB_MATRIX_DRIVER = custom  # use LED custom driver, since COLs are shared
 DEBOUNCE_TYPE = sym_eager_pk
 
